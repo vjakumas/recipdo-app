919a43da43691ed5bbf16d8c2e519aaa6a4f5c9a
vjakumas <viliusjak@gmail.com>
2023-04-25 21:06:48 +0300

    Very gooood changes for Add product and search

--- components/addProduct/AddProduct.js
+++ components/addProduct/AddProduct.js
@@ -1,13 +1,44 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Image, TouchableOpacity, SafeAreaView } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 import styles from "./addProduct.style";
+import { COLORS } from "../../constants";
 
-const AddProductScreen = () => {
+const AddProduct = ({ navigation }) => {
 	return (
 		<View style={styles.container}>
-			<Text style={styles.title}>Add Product Screen</Text>
+			<View style={styles.contentContainer}>
+				<Image source={require("../../assets/images/add-product-illustration.png")} style={styles.logo} />
+				<Text style={styles.mainHeader}>Add a new product</Text>
+				<Text style={styles.subText}>Expand your cooking horizons with a new ingredient!</Text>
+			</View>
+			<View style={styles.bottomContainer}>
+				<TouchableOpacity onPress={() => console.log("Add new product")} style={styles.primaryButton}>
+					<View style={styles.buttonContent}>
+						<Icon name="add-circle" size={24} style={{ marginRight: 10, color: COLORS.white }} />
+						<Text style={styles.primaryButtonText}>Add new product</Text>
+					</View>
+				</TouchableOpacity>
+				<View style={styles.separatorContainer}>
+					<View style={styles.line} />
+					<Text style={styles.separatorText}>Or continue with</Text>
+					<View style={styles.line} />
+				</View>
+				<TouchableOpacity onPress={() => console.log("Scan a product")} style={styles.secondaryButton}>
+					<View style={styles.buttonContent}>
+						<Icon name="scan" size={24} style={{ marginRight: 10, color: COLORS.darkGray }} />
+						<Text style={styles.secondaryButtonText}>Scan a product</Text>
+					</View>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={() => console.log("Scan a barcode")} style={styles.secondaryButton}>
+					<View style={styles.buttonContent}>
+						<Icon name="barcode" size={24} style={{ marginRight: 10, color: COLORS.darkGray }} />
+						<Text style={styles.secondaryButtonText}>Scan a barcode</Text>
+					</View>
+				</TouchableOpacity>
+			</View>
 		</View>
 	);
 };
 
-export default AddProductScreen;
+export default AddProduct;
