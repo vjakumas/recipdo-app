152aeeac9727f02843e89b52a96e723d4d2bd080
vjakumas <viliusjak@gmail.com>
2023-05-15 06:49:55 +0300

    added more tests

--- components/common/cards/productCardMedium/ProductCardMediumList.js
+++ components/common/cards/productCardMedium/ProductCardMediumList.js
@@ -6,6 +6,10 @@ import styles from "./productCardMediumList.style";
 const ProductCardMediumList = ({ products, onPress }) => {
 	const renderItem = ({ item }) => <ProductCardMedium product={item} />;
 
+	if (!products) {
+		return null;
+	}
+
 	return (
 		<ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.productList}>
 			{products.map((product, index) => (
